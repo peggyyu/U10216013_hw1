17b8bcbf8954a5bd7aff35aa712f4307d06cdc35
peggyyu <peggy840225@yahoo.com.tw>
2014-03-07 23:05:06 +0800

    註解
    加上註解

--- src/Function_d.java
+++ src/Function_d.java
@@ -1,9 +1,11 @@
-
+//create Function_d class inherit AbstractDrawFunction
 public class Function_d extends AbstractDrawFunction {
+	//new constructor Function_d
 	public Function_d (){
 		
 	}
 	@Override
+	//concrete method
 	double f(double x) {
 		// TODO Auto-generated method stub
 		return Math.tan(x);
