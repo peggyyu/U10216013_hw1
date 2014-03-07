f806f3c50f88be00b22ed187b27f8ac5a62cde7d
peggyyu <peggy840225@yahoo.com.tw>
2014-03-07 23:01:53 +0800

    註解

--- src/Function_b.java
+++ src/Function_b.java
@@ -1,9 +1,11 @@
-
+//create Function_b class inherit AbstractDrawFunction
 public class Function_b extends AbstractDrawFunction {
+	//new constructor
 	public Function_b (){
 		
 	}
 	@Override
+	//concrete method
 	double f(double x) {
 		// TODO Auto-generated method stub
 		return Math.sin(x);
