719a216493d99f995e36ba3fe53c67728c125694
peggyyu <peggy840225@yahoo.com.tw>
2014-03-07 22:58:40 +0800

    註解
    加上註解

--- src/Function_a.java
+++ src/Function_a.java
@@ -1,9 +1,11 @@
-
+//create Function_a class inherit AbstractDrawFunction
 public class Function_a extends AbstractDrawFunction {
+	//new constructor
 	public Function_a (){
 		
 	}
 	@Override
+	//concrete method
 	double f(double x) {
 		// TODO Auto-generated method stub
 		return Math.pow (x , 2);
