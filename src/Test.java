0cf11c8283c9c8a634595eee63ba8abf08d24530
peggyyu <peggy840225@yahoo.com.tw>
2014-03-09 19:58:47 +0800

    註解
    加上註解

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Test {
 	
+	/** Main method */
 	public static void main(String[] args){
-		
+		//Create a scanner
 		Scanner input = new Scanner(System.in);
+		
+		//Prompt the user to enter x
 		double x = input.nextDouble();
 		
+		//Create seven objects 
 		Function_a aaa = new Function_a();
 		Function_b bbb = new Function_b();
 		Function_c ccc = new Function_c();
@@ -15,6 +19,7 @@ public class Test {
 		Function_f fff = new Function_f();
 		Function_g ggg = new Function_g();
 		
+		//Print out the seven functions answer
 		System.out.println(aaa.f(x));
 		System.out.println(bbb.f(x));
 		System.out.println(ccc.f(x));
